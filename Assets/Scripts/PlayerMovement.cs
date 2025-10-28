54f28be8b6d107c89e55f29d7a43aa62c7956205
minhduong-web <minh.duong@vgl.io.vn>
2025-10-28 16:06:05 +0700

    update

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Cấu hình di chuyển")]
+    public float moveSpeed = 5f; // tốc độ di chuyển
+
+    private Rigidbody2D rb;
+    private float moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Nhận input: A = -1, D = +1
+        moveInput = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        // Di chuyển theo trục X (trái/phải)
+        rb.MovePosition(rb.position + new Vector2(moveInput * moveSpeed * Time.fixedDeltaTime, 0f));
+    }
+}
